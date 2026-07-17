aee242c157f1f465e234b750f2745b03c31b3df9
JiyanshMahajan <surbhimahajan.ca@gmail.com>
2026-07-17 20:24:26 +0530

    Add files via upload

---
+++ art.py
@@ -0,0 +1,50 @@
+class ArtGallery:
+    def __init__(self,gallery_name,location):
+        self.gallery_name=gallery_name
+        self.location=location
+        self.artworks=[]
+        print(f"\nWelcome to{self.gallery_name}!")
+        print(f"Location:{self.location}")
+        print("Art gallery collection is ready.")
+    def add_artwork(self,artwork):
+        self.artworks.append(artwork)
+        print(f"'{artwork}'has been added to the gallery collection.")
+    def remove_artwork(self,artwork):
+        if artwork in self.artworks:
+            self.artworks.remove(artwork)
+            print(f"'{artwork}'has been removed from the gallery collection.")
+        else:
+            print(f"'{artwork}'was not found in the gallery collection.")
+    def display_artworks(self):
+        print(f"\n---{self.gallery_name}Art collection---")
+        if self.artworks:
+            for number,artwork in enumerate(self.artworks,1):
+                print(f"{number}.{artwork}")
+        else:
+            print("No artworks have been added yet.")
+    def __del__(self):
+        print(f"\nClosing{self.gallery_name}.Thank you for managing the art collection!")
+gallery=ArtGallery("Creative Canvas Gallery","Jalandhar")
+while True:
+    print("\nART GALLERY MENU")
+    print("1.Add Artwork")
+    print("2.Remove Artwork")
+    print("3. Display Art Collection")
+    print("4.Exit")
+    print("============================================")
+    choice=input("3:")
+    if choice=="1":
+        artwork_name=input("Mona Lisa:")
+        gallery.add_artwork(artwork_name)
+    elif choice=="2":
+        artwork_name=input("Mona lisa")
+        gallery.remove_artwork(artwork_name)
+    elif choice=="3":
+        gallery.display_artworks()
+    elif choice=="4":
+        print("Exiting the Art Gallery Collection Manager.")
+        del gallery
+        break
+    else:
+        print("Invalid choice.Please enter a number from 1 to 4")
+        
